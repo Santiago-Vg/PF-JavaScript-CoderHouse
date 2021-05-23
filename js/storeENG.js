fb5135ca0e327cb14902f2f17220a8d892e7fa22
SantiagoVega <santi.lcdl@gmail.com>
2021-05-23 19:15:11 -0300

    Tienda con Vanilla JS 2

--- js/storeENG.js
+++ js/storeENG.js
@@ -1,11 +1,9 @@
-// APP TIENDA ONLINE V.5 (con jQuery)
+// APP TIENDA ONLINE V.5 
 
-// DECLARAR CARRITO
 let cart = []
 
-// CARGAR CONTENIDO
-    $(function() {
-        getData()
+    document.addEventListener('DOMContentLoaded', () => {
+        fetchData()
         if (localStorage.getItem('cart')) {
             cart = JSON.parse(localStorage.getItem('cart'))
         }
@@ -13,238 +11,228 @@ let cart = []
         showCartLength()
     })
 
-// VINCULAR PRODUCTOS DESDE API.JSON
-    const getData = () => {
-        $.ajax({
-            method: 'GET',
-            url: '../api.json',
-            success: function(response){
-                const filteredProducts = response.filter(product => product.collection === $('#collections').val())
-                if (filteredProducts.length === 0) {
-                    createCards(response)
-                } else {
-                    createCards(filteredProducts)
-                }
+
+    const searchInput = document.getElementById('product-search')
+    const cartTooltip = document.getElementsByClassName('cart-tooltip')[0]
+    const cards = document.getElementById('cards')
+    const cartModal = document.getElementById('modal-cart')
+    const cartProducts = document.getElementById('items')
+    const cartFooter = document.getElementById('cart-footer')
+    const cartDiscountFooter = document.getElementById('discounts')
+    const cartButtons = document.getElementsByClassName('text-end')[0]
+    const discountsModal = document.getElementById('discApplier')
+    const discCancelBtn = document.getElementById('discBtnCancel')
+    const addProductModal = document.getElementById('addProduct-modal')
+    const thanksModal = document.getElementById('thanks-modal')
+
+
+    const fetchData = async () => {
+        try {
+            const res = await fetch('../api.json')
+            const products = await res.json()
+            const collections = document.getElementById('collections')
+            const filteredProducts = products.filter(el => el.collection === collections.value)
+            if (filteredProducts.length === 0) {
+                createCards(products)
+            } else {
+                createCards(filteredProducts)
             }
-        })
+        } catch (error){
+            console.log(error);
+        }
     }
 
-// MOSTRAR TARJETAS DE PRODUCTOS
     const createCards = (products) => {
-        $('#cards').text('')
+        cards.innerHTML = ''
         products.forEach(element => {
-            $('#cards').append(`
+            cards.innerHTML += `
                 <div class="card col-md-3 col-10 mb-5 align-items-center" id="${element.id}">
                     <img class="store-img img-fluid w-75" src="${element.thumbnailUrl}">
-                    <h2 class="piece-name">${element.titleEng}</h2>
-                    <p class="piece-value">${element.prize}</p>
-                    <button class="btn ver-mas-btn buy" value=${element.id}>Add to Cart</button>
-                </div>`)
+                    <h2 class="piece-name">${element.title}</h2>
+                    <p class="piece-value">${element.price}</p>
+                    <button class="btn ver-mas-btn buy" onclick="addToCart(${element.id}); openModal(addProductModal)">Add to Cart</button>
+                </div>`
         })
     }
 
-    // Filtrar productos
-    $(document).on('click', '#product-filter', function(){
-        getData()
-    })
-
-    // Buscar productos
-    $('#product-search').on('keyup', function(){
-        let value = $(this).val().toLowerCase();
-        $("#cards div").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        })
-    })
-
-// AÑADIR PRODUCTO AL CARRITO
-    //  Encontrar Producto
-        const productIndex = (productId) => {
-            const index = cart.findIndex(element => element.id === productId)
-            return index
-        }
-
-    //  Añadir Producto al Carrito
-        $(document).on('click', '.buy', function(){
-            addToCart(this.value);
-            $('#discounts').attr("style", "display: none;")
-        })
-
-        const addToCart = (elementId) => {
-            $.ajax({
-                method: 'GET',
-                url: '../api.json',
-                success: function(response){
-                    const products = response
-                    const productId = elementId - 1
-                    const cartProduct = {
-                        id: products[productId].id,
-                        title: products[productId].title,
-                        titleEng: products[productId].titleEng,
-                        image: products[productId].thumbnailUrl,
-                        prize: products[productId].prize,
-                        amount: 1
-                    }
-                    if (productIndex(cartProduct.id) >= 0) {
-                        cart[productIndex(cartProduct.id)].amount++
-                    } else {
-                        cart.push(cartProduct)
-                    }
-                    createCart()
-                    showCartLength()
-                }
-            })
-        }
-
- // CREAR CARRITO - Guardarlo en LocalStorage
-    // Abrir y cerrar carrito (modal)
-        $('#cart-icon, .cart-tooltip').on('click', function(){
-            $('#modal-cart').show()
-        })
-
-        $('#modal-cart').on('click', function(event){
-            if(event.target === $('#modal-cart')[0]){
-                $('#modal-cart').hide()
-            } 
-        })
-
-    // Crear lista de productos en el carrito
-        const createCart = () => {
-            $('#items').text('')
-            cart.forEach( (element) => {
-                $('#items')[0].innerHTML += 
-                    `<div>
-                        <tr>
-                            <th scope="row">${element.id}</th>
-                            <td class="prod-td"><img class="cart-img" src="${element.image}" alt="">${element.titleEng}</td>
-                            <td>${element.amount}</td>
-                            <td id="${element.id}">
-                                <button class="add-btn" value="${element.id}">+</button>
-                                <button class="rem-btn" value="${element.id}")>-</button>
-                            </td>
-                            <td><span>$ ${parseFloat(element.amount) * parseFloat(element.prize)}</span></td>
-                        </tr>
-                    </div>`
-            })
-            createFooter()
-            localStorage.setItem('cart', JSON.stringify(cart))
-        }
-    
-    // Mostrar tooltip en el ícono del Carrito
-        const showCartLength = () => {
-            if(cart.length === 0) {
-                $('.cart-tooltip').hide()
+    const search = () => {
+        let searchInputText = searchInput.value.toLowerCase()
+        for (i = 0; i < 16; i++) {
+            let product = cards.getElementsByClassName('card')[i]
+            if (!product.innerHTML.toLowerCase().includes(searchInputText)) {
+                product.style.display="none"
             } else {
-                $('.cart-tooltip').show()
-                $('.cart-tooltip').html(`${cart.reduce((acc, {amount}) => acc + amount, 0)}`)
+                product.style.display=""
             }
         }
+    }
 
-    // Calcular precio del Carrito
-        const createFooter = () => {
-            $('#cart-footer').text('')
-            if (cart.length === 0) {
-                $('#cart-footer').html('<th scope="row" colspan="5">YOUR CART IS EMPTY - Click on "Add to Cart" to start buying!</th>')
-                $('#discounts').hide()
-                $('.text-end').hide()
-                return
-            } else {
-                const nAmount = cart.reduce((acc, {amount}) => acc + amount, 0)
-                const nPrize = cart.reduce((acc, {amount, prize}) => acc + parseFloat(amount) * parseFloat(prize), 0)
-                $('#cart-footer').html(`
-                    <div id="cart-footer">
-                        <th scope="row">TOTAL PRODUCTS</th>
-                        <td>${nAmount}</td>
-                        <td><button class="btn empty-btn btn-sm" id="emptyCart"> empty cart</button></td>
-                        <td>$ <span id="cart-prize">${nPrize}</span></td>
-                    </div>
-                    `)
-                $('#discounts').show()
-                $('.text-end').show()
+    const productIndex = (productId) => {
+        const index = cart.findIndex(element => element.id === productId)
+        return index
+    }
+
+    const addToCart = async (elementId) => {
+        try {
+            const res = await fetch('../api.json')
+            const products = await res.json()
+            const productId = elementId - 1
+            const cartProduct = {
+                id: products[productId].id,
+                title: products[productId].title,
+                titleEng: products[productId].titleEng,
+                image: products[productId].thumbnailUrl,
+                price: products[productId].price,
+                amount: 1
             }
+            if (productIndex(cartProduct.id) >= 0) {
+                cart[productIndex(cartProduct.id)].amount++
+            } else {
+                cart.push(cartProduct)
+            }
+            createCart()
+            showCartLength()
+        } catch (error){
+            console.log(error);
         }
+    }
+
+    const showCart = () => {
+        cartModal.style.display="block"
+    }
+
+    const closeCart = (event) => {
+        if (event.target === cartModal) {
+            cartModal.style.display="none"
+        }
+    }
+
+    const createCart = () => {
+        cartProducts.innerHTML = ''
+        cart.forEach( (element) => {
+            cartProducts.innerHTML += `
+                <div>
+                    <tr>
+                        <th scope="row">${element.id}</th>
+                        <td class="prod-td"><img class="cart-img" src="${element.image}" alt="">${element.titleEng}</td>
+                        <td>${element.amount}</td>
+                        <td id="${element.id}">
+                            <button class="add-btn" onclick=addBTN(${element.id})>+</button>
+                            <button class="rem-btn" onclick=remBTN(${element.id})>-</button>
+                        </td>
+                        <td><span>$ ${parseFloat(element.amount) * parseFloat(element.price)}</span></td>
+                    </tr>
+                </div>`
+        })
         createFooter()
+        localStorage.setItem('cart', JSON.stringify(cart))
+    }
 
-    // Vaciar Carrito
-        $(document).on('click', '#emptyCart', function(){
-            cart = []
-            createCart()
-            showCartLength()
-        })
+    const showCartLength = () => {
+        if(cart.length === 0) {
+            cartTooltip.style.display="none"
+        } else {
+            cartTooltip.style.display="inline"
+            cartTooltip.innerHTML = `${cart.reduce((acc, {amount}) => acc + amount, 0)}`
+        }
+    }
 
-    // Agregar o quitar productos desde el carrito (botones +/-)
-        $(document).on('click', '.add-btn', function(){
-            cartItem = cart[productIndex(parseInt(this.value))]
-            cartItem.amount++
-            createCart()
-            showCartLength()
-            $('#discounts').hide()
-        })
-    
-        $(document).on('click', '.rem-btn', function(){
-            cartItem = cart[productIndex(parseInt(this.value))]
-            cartItem.amount--
-            if (cartItem.amount === 0) {
-                cart.splice(productIndex(parseInt(this.value)),1)
-            }
-            createCart()
-            showCartLength()
-            $('#discounts').hide()
-        })
+    const createFooter = () => {
+        cartFooter.innerHTML = ''
+        if (cart.length === 0) {
+            cartFooter.innerHTML = '<th scope="row" colspan="5">YOUR CART IS EMPTY - Click on "Add to Cart" to start buying!</th>'
+            cartDiscountFooter.style.display="none"
+            cartButtons.style.display="none"
+            return
+        } else {
+            const nAmount = cart.reduce((acc, {amount}) => acc + amount, 0)
+            const nPrice = cart.reduce((acc, {amount, price}) => acc + parseFloat(amount) * parseFloat(price), 0)
+            cartFooter.innerHTML = `
+            <div id="cart-footer">
+                <th scope="row">TOTAL PRODUCTS</th>
+                <td>${nAmount}</td>
+                <td>
+                    <button class="btn empty-btn btn-sm" onclick=emptyCart()> empty cart</button>
+                </td>
+                <td class="font-weight-bold">$ <span id="cart-prize">${nPrice}</span></td>
+            </div>`
+            cartDiscountFooter.style.display=""
+            cartButtons.style.display=""
+        }
+    }
+    createFooter()
 
-// APLICAR DESCUENTOS
-    // Abir y Cerrar modal para aplicar descuentos
-        $('#discModal').on('click', function(){
-            $('#discApplier').show()
-        })
+    const emptyCart = () => {
+        cart = []
+        createCart()
+        showCartLength()
+    }
 
-        $('#discApplier').on('click', function(event){
-            if(event.target === $('#discApplier')[0] || event.target === $('#discBtnCancel')[0]){
-                $('#discApplier').hide()
-            } 
-        })
+    const addBTN = (elementId) => {
+        cartItem = cart[productIndex(elementId)]
+        cartItem.amount++
+        createCart()
+        showCartLength()
+        cartDiscountFooter.style.display="none"
+    }
 
-    // Generar descuentos según cupón
-        $('#discBtnApply').on('click', function(){
-            discGenerator()
-        })
+    const remBTN = (elementId) => {
+        cartItem = cart[productIndex(elementId)]
+        cartItem.amount--
+        if (cartItem.amount === 0) {
+            cart.splice(productIndex(elementId),1)
+        }
+        createCart()
+        showCartLength()
+        cartDiscountFooter.style.display="none"
+    }
 
-        const discGenerator = () => {
-            createCart()
-            let cartPrize =  parseInt($('#cart-prize').text())
-            const checkedDiscount = $('input[name="discount"]:checked').val()
-            const discounts = {
-                'macro': cartPrize * 0.9,
-                'galicia': cartPrize * 0.75,
-                'santander': cartPrize * 0.5,
-                'none': cartPrize
-            }
-            $('#cart-prize').addClass('old-prize')
-            $('#discounts').html(`
-                        <div id="cart-footer">
-                            <td></td>
-                            <td></td>
-                            <th scope="row">Total (Coupon Applied)</th>
-                            <td>$ <span id="new-prize">${discounts[checkedDiscount]}</span></td>
-                        </div>
-                        `)
-            $('#discApplier').hide()
-        }                   
+    const openDiscModal = () => {
+        discountsModal.style.display="block"
+    } 
 
+    const closeDiscModal = (event) => {
+        if(event.target === discountsModal || event.target === discCancelBtn){
+            discountsModal.style.display="none"
+        } 
+        
+    } 
 
-// MODALES DEL SITIO
-    // Modal "Producto Añadido"
-        $(document).on('click', '.buy', function(){
-            $('#addProduct-modal').show()
-        })
-        $(document).on('click', '#addProduct-modal', function(){
-            $('#addProduct-modal').hide()
-        })
+    const discGenerator = () => {
+        createCart()
+        let cartOldValue = document.getElementById('cart-prize')
+        let cartPrice =  parseInt(cartOldValue.innerHTML) 
+        const checkedDiscount = document.querySelector('input[name="discount"]:checked').value
+        const discounts = {
+            'macro': cartPrice * 0.9,
+            'galicia': cartPrice * 0.75,
+            'santander': cartPrice * 0.5,
+            'none': cartPrice
+        }
+        cartOldValue.setAttribute("class", 'old-prize')
+        cartDiscountFooter.innerHTML = `
+            <div id="cart-footer">
+                <td></td>
+                <td></td>
+                <th scope="row">Total (Coupon Applied)</th>
+                <td>$ <span id="new-prize">${discounts[checkedDiscount]}</span></td>
+            </div>
+            `
+        discountsModal.style.display="none"
+    }
 
-    // Modal "Gracias por su Compra"
-        $(document).on('click', '#endPurchase', function(){
-            $('#thanks-modal').show()
-        })
-        $(document).on('click', '#thanks-modal', function(){
-            $('#thanks-modal').hide()
-            $('#modal-cart').hide()
-        })
+    const openModal = (modal) => {
+        modal.style.display="block"
+    }
+
+    const closeModal = (modal) => {
+        modal.style.display="none"
+        cartModal.style.display="none"
+    }
+
+    const closeThanksModal = () => {
+        thanksModal.style.display="none"
+        cartModal.style.display="none"
+        emptyCart()
+        window.location.href = "../index.html";
+    }
